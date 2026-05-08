7bb78b4360c2013637482622ee1584f0b0c949f5
caspa2289 <65313374+caspa2289@users.noreply.github.com>
2026-05-08 21:04:18 +0300

    g-mate: replaced switch statements with if statements after benchmarking

--- packages/benchmarkus/src/benchmarks/vector.ts
+++ packages/benchmarkus/src/benchmarks/vector.ts
@@ -1,4 +1,4 @@
-import { Vector3, Vec } from '@graphenks/g-mate'
+import { Vector3, Vec, Vector2, AnyVector } from '@graphenks/g-mate'
 import { measureAndLog } from '../utils/measureAndLog'
 
 const iterationCount = 1000 * 1000
@@ -11,8 +11,19 @@ const lengthJS = (v0: Vector3) => {
     return Vec.length(v0)
 }
 
+const normalizeOverloaded = (v: Vector2) => {
+    return Vec.normalize(v)
+}
+
+//the difference on average is 0.8ms over 1 million calls. Not worth the typing
+const normalize2 = (v: Vector2) => {
+    const vectorLength = Vec.length(v)
+    return [v[0] / vectorLength, v[1] / vectorLength]
+}
+
 const v0 = Vec.new(4, 5, 6)
 const v1 = Vec.new(1, 2, 3)
+const v2 = Vec.new(1, 4)
 
 export const benchmarkVectorMath = () => {
     measureAndLog(
@@ -29,5 +40,21 @@ export const benchmarkVectorMath = () => {
         },
         iterationCount,
         'Length of vec3 with js impl'
+    )
+
+    measureAndLog(
+        () => {
+            normalizeOverloaded(v2)
+        },
+        iterationCount,
+        'Normalize with overloaded impl'
+    )
+
+    measureAndLog(
+        () => {
+            normalize2(v2)
+        },
+        iterationCount,
+        'Normalize with vec2 impl'
     )
 }
--- packages/g-mate/src/Vector.ts
+++ packages/g-mate/src/Vector.ts
@@ -23,27 +23,26 @@ function cross(v0: Vector2, v1: Vector2): Vector2
 function cross(v0: Vector3, v1: Vector3): Vector3
 function cross(v0: Vector4, v1: Vector4): Vector4
 function cross(v0: AnyVector, v1: AnyVector): AnyVector {
-    //TODO: benchmark switch vs ifelse vs static map
-    switch (v0.length) {
-        case 4: {
-            return [
-                v0[1] * (v1[2] as number) - v0[2] * v1[1],
-                (v1[2] as number) * v1[0] - v0[0] * (v1[2] as number),
-                v0[0] * v1[1] - v0[1] * v1[0],
-                1,
-            ]
-        }
-        case 3: {
-            return [
-                v0[1] * (v1[2] as number) - v0[2] * v1[1],
-                (v1[2] as number) * v1[0] - v0[0] * (v1[2] as number),
-                v0[0] * v1[1] - v0[1] * v1[0],
-            ]
-        }
-        default: {
-            return [0, v0[0] * v1[1] - v0[1] * v1[0]]
-        }
+    //DONE: benchmark switch vs ifelse vs static map
+    //if is on par with separate implementations somehow :D
+    if (v0.length === 4) {
+        return [
+            v0[1] * (v1[2] as number) - v0[2] * v1[1],
+            (v1[2] as number) * v1[0] - v0[0] * (v1[2] as number),
+            v0[0] * v1[1] - v0[1] * v1[0],
+            1,
+        ]
     }
+
+    if (v0.length === 3) {
+        return [
+            v0[1] * (v1[2] as number) - v0[2] * v1[1],
+            (v1[2] as number) * v1[0] - v0[0] * (v1[2] as number),
+            v0[0] * v1[1] - v0[1] * v1[0],
+        ]
+    }
+
+    return [0, v0[0] * v1[1] - v0[1] * v1[0]]
 }
 
 function dot(v0: Vector2, v1: Vector2): number
@@ -66,19 +65,20 @@ function lengthSquared(v: AnyVector): number {
 function normalize(v: AnyVector): AnyVector {
     const vectorLength = length(v)
 
-    switch (v.length) {
-        case 4:
-            return [
-                v[0] / vectorLength,
-                v[1] / vectorLength,
-                v[2] / vectorLength,
-                v[3],
-            ]
-        case 3:
-            ;[v[0] / vectorLength, v[1] / vectorLength, v[2] / vectorLength]
-        default:
-            return [v[0] / vectorLength, v[1] / vectorLength]
+    if (v.length === 4) {
+        return [
+            v[0] / vectorLength,
+            v[1] / vectorLength,
+            v[2] / vectorLength,
+            v[3],
+        ]
     }
+
+    if (v.length === 3) {
+        return [v[0] / vectorLength, v[1] / vectorLength, v[2] / vectorLength]
+    }
+
+    return [v[0] / vectorLength, v[1] / vectorLength]
 }
 
 //Do that instead of static class properties because some sort of constructing still happens for static methods and me no like that
